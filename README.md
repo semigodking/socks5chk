4b8739c88388c590b1debc240c867c62a6ddbdb1
Zhuofei Wang <semigodking@gmail.com>
2023-11-09 14:53:57 +0800

    Capability fix and support verify TCP by DNS request

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 socks5chk - Socks5 Ability Checker
 ----------------------------------
 Simple scripts used for checking SOCKS5 proxy's support of TCP or UDP.
+It could also check if SOCKS5 proxy allows sending DNS to TCP port 53.
 
 Prerequisites
 -------------
 * Python 2.6+ or Python 3.x
 * [PySocks](https://github.com/Anorov/PySocks)
 * [argparse](https://pypi.python.org/pypi/argparse) (required for Python 2.6)
-* [win_inet_pton](https://pypi.python.org/pypi/win_inet_pton) (required for running Python 2.6-3.3 on Windows)
+* [win\_inet\_pton](https://pypi.python.org/pypi/win_inet_pton) (required for running Python 2.6-3.3 on Windows)
 
 Setup
 -----
@@ -23,6 +24,7 @@ Usage
 Run command like this and check output.
 ```shell
 python tcpchk.py -p 192.168.1.1 -P 1080
+python tcpchk.py -p 192.168.1.1 -P 1080 -m dns
 python tcpchk.py -p 192.168.1.1 -P 1080 -u user1 -k password1
 python udpchk.py -p 192.168.1.1 -P 1080
 python udpchk.py -p 192.168.1.1 -P 1080 -u user1 -k password1
--- tcpchk.py
+++ tcpchk.py
@@ -1,12 +1,12 @@
 # tcpchk.py - simple tool to test TCP support of SOCKS5 proxy.
 # Copyright (C) 2016-2017 Zhuofei Wang <semigodking@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License.  You may obtain a copy
 # of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -15,25 +15,58 @@
 
 from __future__ import print_function
 import sys
-if sys.platform == 'win32' and (sys.version_info.major < 3
-                                or (sys.version_info.major == 3 and sys.version_info.minor < 4)):
+
+if sys.platform == 'win32' and (
+    sys.version_info.major < 3
+    or (sys.version_info.major == 3 and sys.version_info.minor < 4)
+):
     # inet_pton is only supported on Windows since Python 3.4
     import win_inet_pton
 import socket
 import socks
 
-def test_tcp(typ, addr, port, user=None, pwd=None):
-    s = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM) # Same API as socket.socket in the standard lib
+
+def verify_by_http(s):
+    # Raw HTTP request
+    host = "www.baidu.com"
+    req = "GET / HTTP/1.0\r\nHost: %s\r\n\r\n" % host
+    s.connect((host, 80))
+    s.send(req.encode("utf-8"))
+    rsp = s.recv(4096)
+    decoded = rsp.decode("utf-8", errors="ignore")
+    return decoded.startswith("HTTP/1.0 200 OK") or decoded.startswith(
+        "HTTP/1.1 200 OK"
+    )
+
+
+def verify_by_dns(s):
+    req = b"\x00\x1b\x12\x34\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x05\x62\x61\x69\x64\x75\x03\x63\x6f\x6d\x00\x00\x01\x00\x01"
+    s.connect(("8.8.8.8", 53))
+    s.send(req)
+    rsp = s.recv(4096)
+    return rsp[2] == req[2] and rsp[3] == req[3]
+
+
+_methods = {
+    "http": verify_by_http,
+    "dns": verify_by_dns,
+}
+
+
+def test_tcp(typ, addr, port, user=None, pwd=None, method="http"):
+    s = socks.socksocket(
+        socket.AF_INET, socket.SOCK_STREAM
+    )  # Same API as socket.socket in the standard lib
     try:
-        s.set_proxy(socks.SOCKS5, addr, port, False, user, pwd) # SOCKS4 and SOCKS5 use port 1080 by default
+        s.set_proxy(
+            socks.SOCKS5, addr, port, False, user, pwd
+        )  # SOCKS4 and SOCKS5 use port 1080 by default
         # Can be treated identical to a regular socket object
-        # Raw HTTP request
-        host = "www.baidu.com"
-        req = b"GET / HTTP/1.0\r\nHost: %s\r\n\r\n" % host
-        s.connect((host, 80))
-        s.send(req)
-        rsp = s.recv(4096)
-        if rsp.startswith("HTTP/1.1 200 OK"):
+        func = _methods.get(method)
+        if not func:
+            print("Bad method: ", method)
+            return
+        if func(s):
             print("TCP check passed")
         else:
             print("Invalid response")
@@ -47,6 +80,7 @@ def test_tcp(typ, addr, port, user=None, pwd=None):
 def main():
     import os
     import argparse
+
     def ip_port(string):
         value = int(string)
         if value <= 0 or value > 65535:
@@ -54,18 +88,54 @@ def main():
             raise argparse.ArgumentTypeError(msg)
         return value
 
-    parser = argparse.ArgumentParser(prog=os.path.basename(__file__), 
-        description='Test SOCKS5 TCP support by sending HTTP request to www.baidu.com and receive response.')
-    parser.add_argument('--proxy', "-p",  metavar="PROXY", dest='proxy', required=True,
-                       help='IP or domain name of proxy to be tested.')
-    parser.add_argument('--port', "-P",  metavar="PORT", dest='port', type=ip_port, default=1080,
-                       help='Port of proxy to be tested.')
-    parser.add_argument('--user', "-u", metavar="username", dest="user", default=None,
-                       help='Specify username to be used for proxy authentication.')
-    parser.add_argument('--pwd', "-k", metavar="password", dest="pwd", default=None,
-                       help='Specify password to be used for proxy authentication.')
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(__file__),
+        description='Test SOCKS5 TCP support by sending HTTP request to www.baidu.com and receive response.',
+    )
+    parser.add_argument(
+        '--proxy',
+        "-p",
+        metavar="PROXY",
+        dest='proxy',
+        required=True,
+        help='IP or domain name of proxy to be tested.',
+    )
+    parser.add_argument(
+        '--port',
+        "-P",
+        metavar="PORT",
+        dest='port',
+        type=ip_port,
+        default=1080,
+        help='Port of proxy to be tested.',
+    )
+    parser.add_argument(
+        '--user',
+        "-u",
+        metavar="username",
+        dest="user",
+        default=None,
+        help='Specify username to be used for proxy authentication.',
+    )
+    parser.add_argument(
+        '--pwd',
+        "-k",
+        metavar="password",
+        dest="pwd",
+        default=None,
+        help='Specify password to be used for proxy authentication.',
+    )
+    parser.add_argument(
+        '--method',
+        "-m",
+        metavar="method",
+        dest="method",
+        default="http",
+        choices=["http", "dns"],
+        help='Test method.',
+    )
     args = parser.parse_args()
-    test_tcp(None, args.proxy, args.port, args.user, args.pwd)
+    test_tcp(None, args.proxy, args.port, args.user, args.pwd, args.method)
 
 
 if __name__ == "__main__":
